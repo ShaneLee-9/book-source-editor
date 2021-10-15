941b7c5aa53c0f17086d51bc5467d1c7b7a199b1
ShaneLee-9 <lixuewen1995@outlook.com>
2021-10-15 11:38:16 +0800

    :heavy_minus_sign: Remove the vite eslint plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import vue from '@vitejs/plugin-vue';
-import { EsLinter, linterPlugin } from 'vite-plugin-linter';
 import { defineConfig } from 'vite';
 import ElementPlus from 'unplugin-element-plus/vite';
 
 export default defineConfig({
   plugins: [
     vue(),
-    linterPlugin({
-      linters: [new EsLinter()],
-    }),
     ElementPlus({
       useSource: true,
     }),
